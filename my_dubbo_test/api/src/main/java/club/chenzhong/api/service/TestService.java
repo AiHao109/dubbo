1190704d73790a35697a454f72b61437bcb46851
chenzhong <1091948791@qq.com>
2020-03-18 11:43:59 +0800

    init commit

---
+++ my_dubbo_test/api/src/main/java/club/chenzhong/api/service/TestService.java
@@ -0,0 +1,10 @@
+package club.chenzhong.api.service;
+
+/**
+ * @program: my_dubbo_test
+ * @description: dubbo api 测试服务接口
+ * @author: aihao
+ * @create: 2020-03-18 11:32:04
+ **/
+public interface TestService {
+}
