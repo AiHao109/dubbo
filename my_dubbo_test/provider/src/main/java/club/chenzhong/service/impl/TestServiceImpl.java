4865fd0f78364298c504767fa64234162de0f1ef
chenzhong <1091948791@qq.com>
2020-03-18 15:06:41 +0800

    init pom2

---
+++ my_dubbo_test/provider/src/main/java/club/chenzhong/service/impl/TestServiceImpl.java
@@ -0,0 +1,12 @@
+package club.chenzhong.service.impl;
+
+
+import club.chenzhong.api.service.TestService;
+/**
+ * @program: my_dubbo_test
+ * @description: test service impl
+ * @author: aihao
+ * @create: 2020-03-18 14:03:08
+ **/
+public class TestServiceImpl implements TestService {
+}
